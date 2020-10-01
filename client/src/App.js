75ee41b9baeea5c367560286a728e17810f44d67
mahak4983 <mahak4983@gmail.com>
2020-10-01 18:55:59 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import M from 'materialize-css';
+const Navbar = () => {
+    useEffect(() => {
+        var elems = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(elems);
+
+    }, []);
+    return (
+        <div>
+            <nav>
+                <div className="nav-wrapper white">
+                    <a href="/" className="brand-logo"><span style={{ color: 'black', marginLeft: '5px' }}>Codemon</span></a>
+                    <a href="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><Link to="/course"><span style={{ color: 'black' }}>Course</span></Link></li>
+                        <li><Link to="/login"><span style={{ color: 'black' }}>Login</span></Link></li>
+                        <li><Link to="/register"><span style={{ color: 'black' }}>Register</span></Link></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul className="sidenav" id="mobile-demo">
+                <li><Link to="/course">Course</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
