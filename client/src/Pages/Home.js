9a64d3e436e382a48e95907993f0e118155a1238
mahak4983 <mahak4983@gmail.com>
2020-10-01 19:51:38 +0530

    element 2

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Element2 from '../Components/Home/Element2';
 import Element4 from '../Components/Home/Element4';
 import Element5 from '../Components/Home/Element5';
 import Footer from '../Components/Home/Footer';
@@ -6,6 +7,7 @@ import Footer from '../Components/Home/Footer';
 const Home = () => {
     return (
         <React.Fragment>
+            <Element2 />
             <Element4 />
             <Element5 />
             <Footer />
