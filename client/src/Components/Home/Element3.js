c1e4591f3b14fe93832f9579ccdc3d5d04533166
mahak4983 <mahak4983@gmail.com>
2020-10-01 21:20:31 +0530

    element 3

---
+++ client/src/Components/Home/Element3.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Binary from "../../Images/Binary.jpg";
+import styles from './Element3.module.css';
+
+const Element3 = () => {
+    return (
+        <div>
+            <br />
+            <div className="container-fluid">
+                <div style={{ marginBottom: '0' }} className={`row ${styles.x}`}>
+                    <div className="col s12 m6 l6">
+                        <h2 className={styles.h2}>We're finally building how developers should learn and connect</h2>
+                        <p className={styles.p}>We agonize over the right ways for people to learn programming and work together as developers so you can focus on just your skills</p>
+                        <button className={styles.btn}>Start Learning</button>
+                    </div>
+                    <div className="col s12 m6 l6">
+                        <img className="fit" src={Binary} alt="Element3" style={{ marginLeft: '30px', marginTop: '18%' }} />
+
+                    </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0a2640" fill-opacity="1" d="M0,224L1440,64L1440,0L0,0Z"></path></svg>
+
+            </div>
+        </div>
+    );
+};
+
+export default Element3;
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Element2 from '../Components/Home/Element2';
+import Element3 from '../Components/Home/Element3';
 import Element4 from '../Components/Home/Element4';
 import Element5 from '../Components/Home/Element5';
 import Footer from '../Components/Home/Footer';
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <React.Fragment>
             <Element2 />
+            <Element3 />
             <Element4 />
             <Element5 />
             <Footer />
